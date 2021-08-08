1db91c6365efa5300f4a680feb12ff7482e983dd
Pradeep <lspradeep21@gmail.com>
2021-08-08 18:44:36 +0530

    updates

--- app/src/main/java/com/testing/android/todo/ui/MainActivity.kt
+++ app/src/main/java/com/testing/android/todo/ui/MainActivity.kt
@@ -8,7 +8,7 @@ import com.testing.android.todo.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findViewById<ComposeView>(R.id.compose_view)?.setContent {
