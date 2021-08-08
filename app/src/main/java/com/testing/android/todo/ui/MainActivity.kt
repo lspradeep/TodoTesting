934228595035a2a1b3b9f4d604b4101166365a88
Pradeep <lspradeep21@gmail.com>
2021-08-08 19:08:05 +0530

    updates

--- app/src/main/java/com/testing/android/todo/ui/MainActivity.kt
+++ app/src/main/java/com/testing/android/todo/ui/MainActivity.kt
@@ -2,8 +2,20 @@ package com.testing.android.todo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.testing.android.todo.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,8 +23,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        findViewById<ComposeView>(R.id.compose_view)?.setContent {
-            Text(text = "hello")
+        val composeView = findViewById<ComposeView>(R.id.compose_view)
+        composeView.setContent {
+            val modifier = Modifier
+            MdcTheme {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Greeting()
+                }
+            }
         }
+    }
+
+    @Composable
+    private fun Greeting() {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
     }
 }
